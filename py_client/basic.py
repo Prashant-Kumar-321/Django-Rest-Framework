4701f90b1cce52c144e9e1ec5537febcc22b50c1
pk123 <pk6176838@gmail.com>
2024-08-01 16:42:23 -0700

    make an api request through python client to the Django project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,15 @@
 import requests 
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything" 
+#endpoint = "https://httpbin.org/status/200"
+#endpoint = "https://httpbin.org/anything" 
+endpoint = "http://localhost:8000/" 
 
 response = requests.get(endpoint, json={"query": "Hello World!"})
 # print(response.text) 
-# print(response.json()) 
-# print(response.status_code)
+print(response.json()) 
+print(response.status_code)
+
+# HTTP Request -> HTML 
+# REST API HTTP Request -> JSON 
+# Javascript Object Notation ~ Python Dict
 
