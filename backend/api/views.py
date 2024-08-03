1531d1a8e5fdafe4f110e0e788e374cf2d4f57a2
pk123 <pk6176838@gmail.com>
2024-08-03 07:52:59 -0700

    Implement model serializer to serialize the model instances

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,18 @@
-import json
-from django.forms.models import model_to_dict
-from django.http import JsonResponse 
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from product.models import Product
+from product.serializers import ProductSerializer 
 
 @api_view(["POST"])
 def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    model_data = Product.objects.all().order_by("?").first()
-    data = {} 
-    if model_data: 
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return Response(data)
+    serializer = ProductSerializer(data=request.data) 
+    if serializer.is_valid(raise_exception=False): 
+        # instance = serializer.save() # save the instance to the data base
+        # print(serializer.data)  
+        return Response(serializer.data)
+    
+    return Response({"data": "Not a valid data"}, status=400)
