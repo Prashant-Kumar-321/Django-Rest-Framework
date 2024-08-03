ab3d1f86018df81ab779868a8d74739b064d13b8
pk123 <pk6176838@gmail.com>
2024-08-02 17:10:10 -0700

    DRF Function based view

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,11 +2,18 @@ import json
 from django.forms.models import model_to_dict
 from django.http import JsonResponse 
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from product.models import Product
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {} 
     if model_data: 
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    return Response(data)
